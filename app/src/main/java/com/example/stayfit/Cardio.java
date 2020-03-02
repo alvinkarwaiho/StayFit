bee0c74e8a4fbbcaddcf9fab5654a6f2f8166568
alvin <alvinkarwaiho123@gmail.com>
2020-03-02 23:49:27 +0000

    Treadmill with data

--- app/src/main/java/com/example/stayfit/Cardio.java
+++ app/src/main/java/com/example/stayfit/Cardio.java
@@ -4,15 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Cardio extends AppCompatActivity {
-
+Button treadmill;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardio);
 
+        treadmill = (Button) findViewById(R.id.treadmill);
 
+        treadmill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Treadmill();
+            }
+        });
+
+    }
+    public void Treadmill(){
+        Intent intent = new Intent(this,Treadmill.class);
+        startActivity(intent);
     }
 }
