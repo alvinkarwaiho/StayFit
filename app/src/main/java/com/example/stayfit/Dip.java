3e899bd4144e0591cfcac94f9500f1837f61c26c
alvin <alvinkarwaiho123@gmail.com>
2020-03-02 13:18:17 +0000

    tricep dip information

--- app/src/main/java/com/example/stayfit/Dip.java
+++ app/src/main/java/com/example/stayfit/Dip.java
@@ -1,13 +1,17 @@
 package com.example.stayfit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Dip extends AppCompatActivity {
     TextView information;
@@ -27,7 +31,57 @@ public class Dip extends AppCompatActivity {
         information1 =(TextView) findViewById(R.id.dips_information1);
         information2=(TextView) findViewById(R.id.dips_information2);
         information3=(TextView) findViewById(R.id.dips_information3);
+     myRef.setValue("At the edge of a bench, grip the seat next to the hip and fingers making sure that the foot is pointed out. Make sure to extend legs and the feet a width apart with heels pointing towards the ground.");
+    myRef1.setValue("Press the palms to lift the body and slide forward enough so that the edge of the chair is behind yourself.");
+     myRef2.setValue("Keeping in mind you should lower yourself between 45 degrees towards 90 degrees also ensuring that your elbows are bent.");
+     myRef3.setValue("Push yourself slowly back up once back to the position started with and repeat for approximately 10 repetitions and more gradually over several weeks’ time.");
+   myRef.addValueEventListener(new ValueEventListener() {
+       @Override
+       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+           String value = dataSnapshot.getValue(String.class);
+           information.setText(value);
+       }
 
+       @Override
+       public void onCancelled(@NonNull DatabaseError databaseError) {
+
+       }
+   });
+   myRef1.addValueEventListener(new ValueEventListener() {
+       @Override
+       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+           String value = dataSnapshot.getValue(String.class);
+           information1.setText(value);
+       }
+       @Override
+       public void onCancelled(@NonNull DatabaseError databaseError) {
+
+       }
+   });
+   myRef2.addValueEventListener(new ValueEventListener() {
+       @Override
+       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+          String value = dataSnapshot.getValue(String.class);
+           information2.setText(value);
+       }
+
+       @Override
+       public void onCancelled(@NonNull DatabaseError databaseError) {
+
+       }
+   });
+   myRef3.addValueEventListener(new ValueEventListener() {
+       @Override
+       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+           String value = dataSnapshot.getValue(String.class);
+           information3.setText(value);
+       }
+
+       @Override
+       public void onCancelled(@NonNull DatabaseError databaseError) {
+
+       }
+   });
     }
 
 
